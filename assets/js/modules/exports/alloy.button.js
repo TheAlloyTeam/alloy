b060233cffd80f36676d01bf600e409281e41e06
Gary Finbow <gary.finbow@gmail.com>
2015-03-17 17:30:06 +0000

    Hovercards and Labels added

--- assets/js/modules/exports/alloy.button.js
+++ assets/js/modules/exports/alloy.button.js
@@ -1,26 +1,52 @@
 (function() {
-    'use strict';
+    var myButton;
 
-    // the ALLOY Namespace is set up in the require config
+    // how to bring in the options
+    var Button = function(element, options) {
+        this.element = element;
+        this.$element = $(element);
+        this.options = options;
+        this.metadata = this.$element.data('options');
+    };
 
-    ALLOY.Button = function() {
+    Button.prototype = {
 
-        // Private Functions
-    	var _init = function() {
-    		ALLOY.Logger.startup('ALLOY.Button Started');
+        that: {},
 
-    	};
+        defaults: {
+            classes: {},
 
-        // Public Interface
-        var public = {
-            init: _init
-        };
+        _init: function() {
+            this.config = $.extend({}, this.defaults, this.options, this.metadata);
 
-        return public;
+            that = this;
 
-    }();
+            ALLOY.Logger.startup('ALLOY.Button Started');
+        },
 
+        _setState: function (state) {
+
+        },
+
+        _handleEvent: function(e) {
+
+        },
+
+        _update: function(id, toOpen) {
+            that.$element.find("." + that.config.classes.title).each(function() { that._handleTitle(this, id, toOpen); });
+        },
+
+    };
+
+    Button.defaults = Button.prototype.defaults;
+
+    $.fn.button = function(options) {
+        return this.each(function() {
+            new Button(this, options)._init();
+        });
+    };
+
+    // Autostart Plugin
     ALLOY.Logger.trace('ALLOY.Button Initializing');
-    // force start of the Library Init function
-    ALLOY.Button.init();
+    //$(".button").button();
 })();
