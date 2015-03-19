211da760989bbb37d79bd325de33f24a69c24cfc
AzzA-D <azza.drink@gmail.com>
2015-03-19 17:30:53 +0000

    Flash js, and a fix in button

--- assets/js/modules/alloy.flash.js
+++ assets/js/modules/alloy.flash.js
@@ -1,14 +1,32 @@
 (function() {
     ALLOY.FlashQueue = function() {
 
-        var queue = [];
+        var flashes = [];
 
         var addFlash = function(flash) {
-            queue.push(flash);
+            flashes.push(flash);
+
+            if (flashes.length == 1) {
+                _displayFlash(flash);
+            }
+        };
+
+        var _displayFlash = function(flash) {
+            $('body').prepend(flash.$flash);
+            flash.$flash.find("." + flash.config.classes.closeButton).one('click', function(e) {
+                e.preventDefault();
+                _closeFlash(flash);
+            });
+        };
+
+        var _closeFlash = function(flash) {
+            flash.$flash.remove();
+            flashes.splice(0, 1);
+            if (flashes.length > 0) { _displayFlash(flashes[0]); }
         };
 
         var public = {
-                        
+            add : addFlash
         }; 
 
         return public;
@@ -24,22 +42,25 @@
         defaults: {
             /***** Worth bearing in mind that mustache.js works quite differently to some other interpretations of handlebars so doesn't need if or each calls - worth changing version? *****/
             template: 
-                '<div class="flash{{#item.modifier}} flash--{{item.modifier}}{{/item.modifier}}{{#item.status}} {{item.status}}{{/item.status}}">' +
+                '<div class="flash example{{#item.modifier}} flash--{{item.modifier}}{{/item.modifier}}{{#item.status}} {{item.status}}{{/item.status}}">' +
                     '<a href="#" class="flash__close">&times;</a>' +
-                    '{{#content.icon}}' +
-                    '<div class="flash__icon">' +
+                    '<div class="flash__icon">{{#content.icon}}' +
                         '{{#item.modifier}}<i class="icon icon--{{item.modifier}}{{#item.size}} {{item.size}}{{/item.size}}"{{#content.title}} title="{{content.title}}"{{/content.title}} aria-hidden="true" role="img"></i>{{/item.modifier}}' +
-                    '</div>' +
-                    '{{/content.icon}}' +
+                    '{{/content.icon}}</div>' +
                     '<div class="flash__content">{{content.text}}</div>' +
                 '</div>',
             classes: {
                 'closeButton': 'flash__close',
             }
         },
+
+        $flash: {},
+
         _init: function(json) { 
             this.config = $.extend({}, this.defaults, this.options, undefined);
-
+            var html = window.Mustache.render(this.config.template, json);
+            this.$flash = $($.parseHTML(html));
+            ALLOY.FlashQueue.add(this);
         },
     };
 
